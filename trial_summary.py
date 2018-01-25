c3c373a89b90b37d036df14b3655d84c3911925c
pannagashree <skandha98@gmail.com>
2018-01-25 23:36:00 +0530

    changed input from url to corpus file

--- trial_summary.py
+++ trial_summary.py
@@ -4,11 +4,10 @@ from nltk import word_tokenize
 from bs4 import BeautifulSoup
 import urllib
 from nltk.corpus import stopwords
-url= "https://rare-technologies.com/text-summarization-in-python-extractive-vs-abstractive-techniques-revisited/"
-response=urllib.urlopen(url)			#opens the url
-raw=response.read().decode('utf8')		#reads the content from the url and uses utf8 encoding to decode
+fh=open('/home/pannaga/bel project/corpus/citations_class/06_1.xml','r');
+raw=fh.read().decode('utf8')		#reads the content from the file and uses utf8 encoding to decode
 #print raw
-soup=BeautifulSoup(raw,"html5lib")		#to remove html tags,beautifulsoup is used
+soup=BeautifulSoup(raw,"lxml")		#to remove html tags,beautifulsoup is used
 #print soup					
 html_free=soup.get_text(strip=True)		#to get only the text from the url removing all the tags
 #print html_free
